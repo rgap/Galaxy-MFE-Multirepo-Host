3834610e9a0d6a32ace1aef52e4afddf1b385964
rgap <r.guzmanap@gmail.com>
2025-06-09 14:32:43 -0500

    fix: update because of new common components
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   configs/webpack.dev.js
    	modified:   package.json
    	modified:   src/pages/Home.js

--- configs/webpack.dev.js
+++ configs/webpack.dev.js
@@ -1,25 +1,23 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const webpackBaseConfig = require('./webpack.base');
+const path = require("path");
+const { merge } = require("webpack-merge");
+const webpackBaseConfig = require("./webpack.base");
 
 module.exports = merge(webpackBaseConfig, {
-  mode: 'development',
-  devtool: 'eval-source-map',
+  mode: "development",
+  devtool: "eval-source-map",
   cache: false,
   optimization: {
     minimize: false,
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, '../dist'),
-    },
-    headers: {
-      'Access-Control-Allow-Origin': '*',
+      directory: path.join(__dirname, "../dist"),
     },
     port: 3000,
     hot: true,
-    open: true,
-    compress: false,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     historyApiFallback: true,
   },
-}); 
+});
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// ... existing code ...
+import { Button } from "common-components-react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div style={{
-      textAlign: 'center',
-      padding: '2rem'
-    }}>
-      <h1 style={{ marginBottom: '2rem' }}>Welcome to Our Store</h1>
-      <p style={{ fontSize: '1.2rem', marginBottom: '2rem' }}>
-        Discover our amazing products and great deals!
-      </p>
-      
-      <div style={{
-        display: 'flex',
-        gap: '2rem',
-        justifyContent: 'center',
-        marginTop: '2rem'
-      }}>
-        <Link
-          to="/catalog"
-          style={{
-            padding: '1rem 2rem',
-            backgroundColor: '#007bff',
-            color: 'white',
-            textDecoration: 'none',
-            borderRadius: '4px',
-            fontSize: '1.1rem'
-          }}
-        >
-          Browse Catalog
+    <div
+      style={{
+        textAlign: "center",
+        padding: "2rem",
+      }}
+    >
+      <h1 style={{ marginBottom: "2rem" }}>Welcome to Our Store</h1>
+      <p style={{ fontSize: "1.2rem", marginBottom: "2rem" }}>Discover our amazing products and great deals!</p>
+
+      <div
+        style={{
+          display: "flex",
+          gap: "2rem",
+          justifyContent: "center",
+          marginTop: "2rem",
+        }}
+      >
+        <Link to="/catalog" style={{ textDecoration: "none" }}>
+          <Button className="btn-primary">Browse Catalog</Button>
         </Link>
-        
       </div>
     </div>
   );
 };
 
-export default Home; 
+export default Home;
